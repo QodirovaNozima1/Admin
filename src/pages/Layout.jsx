ded65f26ba3b3dd2164c787118a169ae5effe509
Nozima <nozimaqodirova07@gmail.com>
2025-02-16 23:37:11 +0500

    finish

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../Components/Header'
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
+import Footer from '../Components/Footer'
 
 const Layout = () => {
   return (
